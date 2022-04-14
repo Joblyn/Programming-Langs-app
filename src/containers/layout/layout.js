9cc4a85e611ebe9f3bca81de9923ad7a99a56589
JobOaikhenah <job.oaikhenah@whogohost.com>
2022-04-14 13:38:30 +0100

    update

--- src/containers/layout/layout.js
+++ src/containers/layout/layout.js
@@ -7,7 +7,7 @@ import { Javascript, Python, Java, Name } from "../../pages";
 export default function Layout() {
   return (
     <main className="flex flex-row">
-      <div className="flex flex-row justify-center md:justify-start xl:pt-[134px] xl:pr-[84px] xl:pb-[150px] xl:pl-[151px] lg:pt-[124px] lg:px-[80px] lg:pb-[127px] sm:pt-[133px] sm:px-125px sm:pb-0 pt-[52px] px-[50px] pb-0 page_container w-full md:w-[60%] min-h-screen">
+      <div className="flex flex-row justify-center md:justify-start xl:pt-[50px] xl:pr-[84px] xl:pb-[150px] xl:pl-[151px] lg:pt-[50px] lg:px-[80px] lg:pb-[127px] sm:pt-[50px] sm:px-125px sm:pb-0 pt-[50px] px-[50px] pb-0 page_container w-full md:w-[60%] min-h-screen">
         <Routes>
           <Route path={ROUTES.NAME} exact element={<Name />} />
           <Route path={ROUTES.JAVA} exact element={<Java />} />
