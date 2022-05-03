4178dc48839b1fd905379031cb14482c64ae446a
JobOaikhenah <job.oaikhenah@whogohost.com>
2022-05-03 01:12:12 +0100

    update on question component test

--- src/__tests__/components/question.test.js
+++ src/__tests__/components/question.test.js
@@ -11,6 +11,7 @@ describe("<Question />", () => {
     text: "Do yo know Javascript objects?",
     choices: "Yes|No|Maybe",
   };
+  const defaultValue = "Yes";
   // UI test
   it("renders component correctly", () => {
     const { container, getByText } = render(
@@ -23,6 +24,20 @@ describe("<Question />", () => {
     expect(getByText("Yes")).toBeTruthy();
     expect(getByText("No")).toBeTruthy();
     expect(getByText("Maybe")).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  // renders with preselected option
+  it("renders with defaultValue prop", () => {
+    const { container, getByText, getByTestId } = render(
+      <Provider store={store}>
+        <Question question={question} defaultValue={defaultValue} />
+      </Provider>
+    );
+
+    expect(getByTestId("CheckOutlinedIcon").parentElement).toBe(
+      getByText("Yes").parentElement
+    );
     expect(container.firstChild).toMatchSnapshot();
   });
 });
